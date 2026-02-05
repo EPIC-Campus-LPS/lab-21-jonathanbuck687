07850c0b386a1d0402736aa55e24fc94bd18fa6f
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-02-05 11:27:18 -0700

    not doen

--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -29,7 +29,41 @@ public class ReversePolishNotation {
         String output = "";
         String numbers = "0123456789";
         for (int i = 0; i < input.length() - 1; i++) {
-            if (numbers.contains(input.substring(i, i + 1)));
+            if (numbers.contains(input.substring(i, i + 1))) {
+                output += input.substring(i, i + 1);
+            }
+            else if (input.substring(i, i + 1).equals("+")) {
+                s.push(0);
+            }
+            else if (input.substring(i, i + 1).equals("-")) {
+                s.push(1);
+            }
+            else if (input.substring(i, i + 1).equals("/")) {
+                if (s.peek() < 2) {
+                    output += input.substring(i, i + 1);
+                    if (s.peek() == 1) {
+                        s.pop();
+                        output += "-";
+                    }
+                    else if (s.peek() == 0) {
+                        s.pop();
+                        output += "+";
+                    }
+                }
+            }
+            else if (input.substring(i, i + 1).equals("*")) {
+                if (s.peek() < 2) {
+                    output += input.substring(i, i + 1);
+                    if (s.peek() == 1) {
+                        s.pop();
+                        output += "-";
+                    }
+                    else if (s.peek() == 0) {
+                        s.pop();
+                        output += "+";
+                    }
+                }
+            }
         }
     }
 }
